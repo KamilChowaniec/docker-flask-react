7ec67afa14200817a2dd76fa17e3e07286a93a9d
Kamil Chowaniec <kamilchowaniec97@gmail.com>
2019-07-28 20:43:48 +0200

    Initial commit

---
+++ FrontEnd/src/src/components/App/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = { info: '' }
+
+  componentDidMount() {
+    fetch('/elo')
+      .then(res => res.text())
+      .then(data => this.setState({ info: data }))
+      .catch(err => { })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h4>{this.state.info}</h4>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
+ 
